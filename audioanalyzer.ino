e5584a501fcb5adeed3570704254a4b98f884959
Tobias Mitterfellner <t.mitti@web.de>
2019-06-12 20:34:12 +0200

    Interrupt 1Hz toggles pin 13

--- audioanalyzer.ino
+++ audioanalyzer.ino
@@ -17,7 +17,7 @@ void setup() {
   TCCR1B = 0;             // same for TCCR1B
   TCNT1  = 0;             // initialize counter value to 0
   // set compare match register for 1hz increments
-  OCR1A = (16*10^6) / (1*1024) - 1 // Set Divider to 40kHz(must be <65536)
+  OCR1A = (16*10^6) / (1*1024) - 1 // Set Divider to 1Hz(must be <65536)
   TCCR1B |= (1 << WGM12); // turn on CTC mode
   TCCR1B |= (1 << CS12) | (1 << CS10);    // Set CS10 and CS12 bits for 1024 prescaler
   TIMSK1 |= (1 << OCIE1A);  // enable timer compare interrupt
