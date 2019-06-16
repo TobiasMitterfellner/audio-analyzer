b42427589412c246d9ff26b9987394229caa63c6
Tobias Mitterfellner <t.mitti@web.de>
2019-06-16 22:07:39 +0200

    Interrupt 40kHz added

--- audioanalyzer.ino
+++ audioanalyzer.ino
@@ -11,35 +11,40 @@ void setup() {
   pinMode(2, OUTPUT);
   pinMode(13, OUTPUT);
   
-  cli();//stop interrupts
+  // SRC: https://www.instructables.com/id/Arduino-Timer-Interrupts/
+  cli();                                  //stop interrupts
 
-  /*//set timer0 interrupt at 2kHz
-  TCCR0A = 0;// set entire TCCR0A register to 0
-  TCCR0B = 0;// same for TCCR0B
-  TCNT0  = 0;//initialize counter value to 0
-  // set compare match register for 2khz increments
-  OCR0A = 124;// = (16*10^6) / (2000*64) - 1 (must be <256)
-  // turn on CTC mode
-  TCCR0A |= (1 << WGM01);// Set CS01 and CS00 bits for 64 prescaler
-  TCCR0B |= (1 << CS01) | (1 << CS00);   // enable timer compare interrupt
-  TIMSK0 |= (1 << OCIE0A);*/
+  //set timer0 interrupt at 40kHz
+  TCCR0A = 0;                             // set entire TCCR0A register to 0
+  TCCR0B = 0;                             // same for TCCR0B
+  TCNT0  = 0;                             //initialize counter value to 0
+  // set compare match register for 40khz increments
+  OCR0A = 49;                             // = (16*10^6) / (40000*8) - 1 (must be <256)
+  TCCR0A |= (1 << WGM01);                 // turn on CTC mode
+  TCCR0B |= (1 << CS01);    // Set CS01(=1) bit for 8 prescaler
+  TIMSK0 |= (1 << OCIE0A);                // enable timer compare interrupt
   
   // set timer1 interrupt at 1Hz
-  TCCR1A = 0;             // set entire TCCR1A register to 0
-  TCCR1B = 0;             // same for TCCR1B
-  TCNT1  = 0;             // initialize counter value to 0
+  TCCR1A = 0;                             // set entire TCCR1A register to 0
+  TCCR1B = 0;                             // same for TCCR1B
+  TCNT1  = 0;                             // initialize counter value to 0
   // set compare match register for 1hz increments
   OCR1A = 15624;                          // = (16*10^6) / (1*1024) - 1 (must be <65536)  
   TCCR1B |= (1 << WGM12);                 // turn on CTC mode
-  TCCR1B |= (1 << CS12) | (1 << CS10);    // Set CS10 and CS12 bits for 1024 prescaler
+  TCCR1B |= (1 << CS12) | (1 << CS10);    // Set CS10(=0) and CS12(=2) bits for 1024 prescaler 
   TIMSK1 |= (1 << OCIE1A);                // enable timer compare interrupt
 
   //DDRC = DDRC | B11111110; // Set Pin A0 input
   sei();                                  // allow interrupts
 }
 
-// Interrupt-handler timer1
-ISR(TIMER1_COMPA_vect){//timer1 interrupt 1Hz toggles pin 13 (LED)
+// Interrupt-handler timer0 @40kHz
+ISR(TIMER0_COMPA_vect){
+// Read A1
+}
+
+// Interrupt-handler timer1 @1Hz
+ISR(TIMER1_COMPA_vect){
 //generates pulse wave of frequency 1Hz/2 = 0.5kHz (takes two cycles for full wave- toggle high then toggle low)
   if (toggle1){
     digitalWrite(13,HIGH);
@@ -52,11 +57,12 @@ ISR(TIMER1_COMPA_vect){//timer1 interrupt 1Hz toggles pin 13 (LED)
 }
                                                                                                                                                          
 void loop() {
-  /*Serial.print("\nCS00 = ");
-  Serial.print(CS00);
-  Serial.print(", CS01 = ");
-  Serial.print(CS01);
-  delay(1000);*/
+  if (toggle1){
+    Serial.print(", CS10 = ");
+    Serial.print(CS10);
+        Serial.print(", CS12 = ");
+    Serial.print(CS12);
+  }
   audio = analogRead(audiopin);
   audioString = "";
   int i = 0;
@@ -70,5 +76,5 @@ void loop() {
   else{
     digitalWrite(2,LOW);
   }
-  Serial.println(audioString);
+  //Serial.println(audioString);
 }
